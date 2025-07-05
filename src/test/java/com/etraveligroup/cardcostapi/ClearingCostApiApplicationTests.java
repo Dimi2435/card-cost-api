12e9d7b584ddb0af50951a6b69761045d634697d
dimitrios.milios <dimitrios.milios@securix.swiss>
2025-07-06 00:39:47 +0200

    Adding jacoco in configuration and clean up duplicate test.

--- src/test/java/com/etraveligroup/cardcostapi/ClearingCostApiApplicationTests.java
+++
@@ -1,4 +0,0 @@
-// This file has been renamed to ClearingCostApiApplicationTests.java
-// Author: Dimitrios Milios
-// Unit tests for the ClearingCostApiApplication.
-// This class contains tests for the main application.
